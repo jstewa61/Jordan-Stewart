8fd35548613c1421e8a54b90a2275ae8c5cb804d
jstewa61 <jstewa61@kent.edu>
2019-06-20 22:16:31 -0400

    animation

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-var animation = bodymovin.loadAnimation({
-container: document.getElementById('anim'),
-renderer: 'svg',
-loop: true,
-autoplay: true,
-path: 'data.json'
-})
+
 
 
 $(document).ready(function() {
